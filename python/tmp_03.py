54d8153bea6111f02a62539f9b82e125fe3ec05a
mlunacek <monte.lunacek@gmail.com>
2013-10-10 10:22:52 -0600

    solutions

---
+++ python/tmp_03.py
@@ -0,0 +1,25 @@
+import os, csv
+data_path = os.path.join('data','titanic.csv')
+print data_path
+
+# How many rows?
+#------------------------------------------------------------------------
+   
+
+# What percent of the people survived?
+#------------------------------------------------------------------------
+
+
+
+# Write a function that returns a dictionary of the number of survived, 
+# not survived, and unknown.
+#------------------------------------------------------------------------
+
+
+
+#What percent of males survived? Females?
+#------------------------------------------------------------------------
+
+
+
+
